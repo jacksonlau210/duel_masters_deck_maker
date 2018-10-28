331bc6028957e4e1af43ba8cea2d132acc15bd7b
jacksonlau210 <jacksonlau210@gmail.com>
2018-10-28 00:21:20 -0400

    Updates

---
+++ precache-manifest.a084ea3ba589ccad0a6f29e698a3fe38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/duel_masters_deck_maker/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "626e661a93d6d7e5fb56",
+    "url": "/duel_masters_deck_maker/static/js/runtime~main.626e661a.js"
+  },
+  {
+    "revision": "d8206c4ac555c3688240",
+    "url": "/duel_masters_deck_maker/static/js/main.d8206c4a.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/duel_masters_deck_maker/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "d8206c4ac555c3688240",
+    "url": "/duel_masters_deck_maker/static/css/main.fd8e881c.chunk.css"
+  },
+  {
+    "revision": "ff99257f048bfe8d9179c08c86c5aaaf",
+    "url": "/duel_masters_deck_maker/index.html"
+  }
+];

012e92e108aed0203c761c4bdd8a876622b2daf8
jacksonlau210 <jacksonlau210@gmail.com>
2019-01-21 22:18:11 -0500

    Updates

---
+++ precache-manifest.7ab6860dde48c56d24cc98de4b7c7719.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2966c5b90c810996d1a91b774b28013f",
+    "url": "/duel_masters_deck_maker/static/media/login_background.2966c5b9.jpg"
+  },
+  {
+    "revision": "605740b11a1965470d1ca6ab8c79923a",
+    "url": "/duel_masters_deck_maker/static/media/dm_logo.605740b1.png"
+  },
+  {
+    "revision": "3bf9ba8eb8b4466d0a753324f1e40799",
+    "url": "/duel_masters_deck_maker/static/media/dm__icon.3bf9ba8e.png"
+  },
+  {
+    "revision": "9b7b9245ea194ffd5e9d6d74c3a2e037",
+    "url": "/duel_masters_deck_maker/static/media/deck_case.9b7b9245.png"
+  },
+  {
+    "revision": "626e661a93d6d7e5fb56",
+    "url": "/duel_masters_deck_maker/static/js/runtime~main.626e661a.js"
+  },
+  {
+    "revision": "b977d2ed57e55bb23b8b",
+    "url": "/duel_masters_deck_maker/static/js/main.b977d2ed.chunk.js"
+  },
+  {
+    "revision": "7e88a6587de1e537c0e8",
+    "url": "/duel_masters_deck_maker/static/js/1.7e88a658.chunk.js"
+  },
+  {
+    "revision": "b977d2ed57e55bb23b8b",
+    "url": "/duel_masters_deck_maker/static/css/main.0fd3fa1f.chunk.css"
+  },
+  {
+    "revision": "1d6b6eb28c6b577ed3a3d9183a1ad78b",
+    "url": "/duel_masters_deck_maker/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/duel_masters_deck_maker/precache-manifest.9151dd5296ecbb3bef1bea2d7f4d3a06.js"
+  "/duel_masters_deck_maker/precache-manifest.7ab6860dde48c56d24cc98de4b7c7719.js"
 );
 
 workbox.clientsClaim();
